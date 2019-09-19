59e06fc0e81d181464fc5baa4f4591976dbbed21
Kavleer <chriskabler@yahoo.com>
2019-09-19 08:20:42 -0400

    Basework, ill

---
+++ main.js
@@ -0,0 +1,38 @@
+const inputButton = document.querySelector('.input')
+const operationButton = document.querySelector('.operation')
+const equalButton = document.querySelector('#equal')
+
+const clearButton = document.querySelector('#clearButn')
+
+
+
+// inputButton.addEventListener('click', function (event) {
+//   const field = document.querySelector('.input')
+//   const entry = innerText.value
+
+
+//   if (entry) {
+//     const newEntry = document.createElement('p')
+//     newEntry.innerText = entry + entry
+//     // console.log(newEntry.innerText)
+//     console.log("hi")
+//     }
+
+//     document.querySelector('#display').appendChild(newEntry.innerText)
+//   })
+
+
+
+  clearButton.addEventListener('click', function (event) {
+    const field = document.querySelector('#clearButn')
+    
+    // if (entry) {
+    //   const newEntry = document.createElement('p')
+    //   newEntry.innerText = entry + entry
+    //   // console.log(newEntry.innerText)
+      console.log("hi")
+    //   }
+  
+    //   document.querySelector('#display').appendChild(newEntry.innerText)
+    })
+  
